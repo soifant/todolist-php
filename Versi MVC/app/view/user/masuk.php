5be78e2e1ebec543b195cf4088faa259df48901f
soifant <31540006+soifant@users.noreply.github.com>
2022-08-17 10:39:47 +0700

    Menambahkan view masuk.php

---
+++ Versi MVC/app/view/user/masuk.php
@@ -0,0 +1,8 @@
+<h1>Silahkan login, untuk membuat toDoList atau <a href="/?url=user/index/daftar" >Daftar</a></h1>
+<form action="/?url=user/masuk" method="post" >
+<input name="user" placeholder="username" required="required">
+<br>
+<input name="pass" placeholder="password" required="required" >
+<br>
+<button type="submit">Login</button>
+</form>
