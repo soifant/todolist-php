3283fac83a9c472dff47afcf00c7113a5e6293ea
soifant <31540006+soifant@users.noreply.github.com>
2022-08-17 10:40:26 +0700

    Menambahkan view daftar.php

---
+++ Versi MVC/app/view/user/daftar.php
@@ -0,0 +1,8 @@
+<h1>Silahkan dafatr, untuk membuat toDoList atau <a href="/?url=user/index/login" >Login</a></h1>
+<form action="/?url=user/daftar" method="post" >
+<input name="user" placeholder="username" required="required">
+<br>
+<input name="pass" placeholder="password" required="required" >
+<br>
+<button type="submit">Daftar</button>
+</form>
