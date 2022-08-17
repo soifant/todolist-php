ef144f4dea6c5c3b4eae98d36459c3c67a82d82a
soifant <31540006+soifant@users.noreply.github.com>
2022-08-18 06:28:00 +0700

    Menambahkan alert ()

--- Versi MVC/app/core/controller.php
+++ Versi MVC/app/core/controller.php
@@ -2,12 +2,29 @@
 
 class controller{
 	public function view($view, $data=[]){
-	        require_once 'app/view/'.$view.'.php';
+		
+		require_once 'app/view/'.$view.'.php';
 	}
 	
 	public function model($model){
+		
 		require_once 'app/model/'.$model.'.php';
 		return new $model;
+	}
+	
+	public function alert(){
+	
+			$ses = $_SESSION['alert'];
+		
+			if($ses){
+				unset($_SESSION['alert']);
+				if($ses[0] == 'gagal'){
+					echo $ses[1]." gagal";
+				
+				}else{
+					echo $ses[1]." berhasil";
+			}
+		}
 	}
 	
 
