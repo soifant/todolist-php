01d15430e362c66b7a92fe27a771111483b7eb54
soifant <31540006+soifant@users.noreply.github.com>
2022-08-17 10:35:50 +0700

    Menambahkan controller user.php

---
+++ Versi MVC/app/controller/user.php
@@ -0,0 +1,40 @@
+<?php
+
+class user extends Controller{
+	public function index($param){
+	
+	switch($param){
+		case 'masuk':
+			$this->view('user/masuk');
+				break;
+			
+		case 'daftar':
+			$this->view('user/daftar');
+				break;
+				
+		default:
+			$this->view('user/masuk');
+				break;
+	}
+	
+	
+	}
+	
+	public function daftar(){
+		$this->model('User_model')->getDaftar($_POST);
+	}
+	
+	public function masuk(){
+		$this->model('User_model')->getMasuk($_POST);
+	
+		
+	}
+	
+	public function keluar(){
+		session_unset();
+		session_destroy();
+		header('location:/');
+	}
+
+}
+?>
