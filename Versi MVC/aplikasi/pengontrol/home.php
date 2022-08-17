d4eefb85c256f26a8b11fa79fa7e8093f4d6943f
soifant <31540006+soifant@users.noreply.github.com>
2022-08-17 10:11:21 +0700

    Memperbarui home.php

--- Versi MVC/aplikasi/pengontrol/home.php
+++ Versi MVC/aplikasi/pengontrol/home.php
@@ -1,1 +1,40 @@
+<?php
 
+class home extends controller{
+
+	public function index(){
+		$data['data'] = $this->model('Home_model')->getHome();
+		$this->view('home/home', $data);
+	}
+	
+	public function hapus($list){
+		$this->model('Home_model')->getHapus($list);
+	}
+	
+	public function tambah(){
+		$this->model('Home_model')->getTambah($_POST);
+	
+	}
+	
+	public function selesai(){
+		$data['data'] = $this->model('Home_model')->getSelesai();
+		$this->view('home/home', $data);
+	}
+	
+	public function menunggu(){
+		$data['data'] = $this->model('Home_model')->getMenunggu();
+		$this->view('home/home', $data);
+	}
+	
+	public function selesaikan($list){
+		$this->model('Home_model')->getSelesaikan($list);
+	
+	}
+	
+	public function cari(){
+		$data['data'] = $this->model('Home_model')->getCari($_POST);
+		$this->view('home/home', $data);
+	}
+}
+
+?>
