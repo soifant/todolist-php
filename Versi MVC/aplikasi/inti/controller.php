f88bd43b66105b9f33e483bd2539107777494bbb
soifant <31540006+soifant@users.noreply.github.com>
2022-08-17 10:13:22 +0700

    Memperbarui controller.php

--- Versi MVC/aplikasi/inti/controller.php
+++ Versi MVC/aplikasi/inti/controller.php
@@ -1,1 +1,29 @@
+<?php
 
+class controller{
+	public function view($view, $data=[]){
+		
+		if($_SESSION['user']){
+		
+			require_once 'app/view/'.$view.'.php';
+		
+		}else if($view == 'user/daftar'){
+		
+			require_once 'app/view/'.$view.'.php';
+		
+		}else{
+		
+			require_once 'app/view/user/masuk.php';
+		
+		}
+	}
+	
+	public function model($model){
+		require_once 'app/model/'.$model.'.php';
+		return new $model;
+	}
+	
+
+}
+
+?>
