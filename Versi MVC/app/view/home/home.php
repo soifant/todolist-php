71e9e46b7744407b3c78d3a466326343c1ecb154
soifant <31540006+soifant@users.noreply.github.com>
2022-08-19 12:51:08 +0700

    Mengubah tampilan

--- Versi MVC/app/view/home/home.php
+++ Versi MVC/app/view/home/home.php
@@ -1,100 +1,108 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta content='width=device-width, initial-scale=1' name='viewport'/>
-<title>Aplikasi web sederhana ToDolist PHP</title>
-<style type="text/css">
-#header{
-	border:1px solid #000;
-	padding:10px;
-	background:black;
-	color:white;
-	margin-bottom:10px;
-}
+<div class="container">
 
-.tambah{
-	float:right;
-}
+<div class="row border border-dark shadow-sm rounded m-1 mb-5 fs-3">
+<i class="fa fa-user-circle col-9 p-2"><?= $_SESSION['user']; ?></i>
 
-#list{
-	border:1px solid gray;
-	padding:5px;
-}
-</style>
-</head>
-<body>
-
-<div id="header">
-<span class="nama">
-Todolist
-</span>
-
-<a href="#" onclick="tambah()">
-<span class="tambah">
-Tambah
-</span></a>
+<div class="col-2 pt-1 fs-4">
+<div class="row">
+<a href="#" class="text-decoration-none col-6" data-bs-toggle="modal" data-bs-target="#keluar">
+<i class="fa fa-plus-square success">List</i>
+</a>
 
 </div>
-
-<?php echo "<br>Halo ". $_SESSION['user']; ?>
-
-<br>
-<br>
-<div id="tambah" style="display:none;">
-<form action="/?url=home/tambah" method="post" >
-<input name="list" placeholder="List name" required="required">
-<button type="submit">Tambah</button>
-</form>
+</div>
+<!---->
 </div>
 
-<a href="/?url=home/selesai">Selesai</a>
-<br>
-<a href="/?url=home/menunggu" >Menunggu</a>
-<br>
-<a href="/" >Semua</a>
-<br>
-<a href="/?url=user/keluar" >Keluar</a>
-<br>
+<div class="modal fade" id="keluar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambahkan list</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+       <form method="post"	action="/?url=home/tambah">
+       <div class="input-group">
+       <input type="text" class="form-control" name="list" placeholder="Nama list" aria-label="Tambah data" aria-describedby="button-addon2">
+       <button class="btn btn-outline-secondary btn-success black" type="submit" id="button-addon2">Tambah</button>
+       
+       </div>
+       </form></div>
+    </div>
+  </div>
+</div>
 
 
-<form method="post" action="/?url=home/cari">
-<input placeholder="input" name="cari" >
-<button type="submit">cari</button>
-</form>
-<br>
-<br>
+<div class="fs-4">
+<i class="fa fa-list-alt">
+Daftar list anda
+</i>
+</div>
 
 <?php 
+if(!empty($data['data'])){
 foreach($data['data'] as $da){
-
-echo '
-<div id="list">
-
-<span>
-'.$da['list'].'
-</span>
-<span class="tambah">
-';
-
-if($da['status'] == 'menunggu'){
-	echo '<a href="/?url=home/selesaikan/'.$da['list'].'"> Selesaikan</a>';
-}else{
-	echo $da['status'];
-}
-
-echo '
-<a href="/?url=home/hapus/'.$da['list'].'"> Hapus</a>
-
-</span>
-
-</div>';
-}
 ?>
+<div class="row bg-light p-1 m-1 border border-dark rounded">
+  <div class="col-7 pt-1">
+  <span class="fs-6 text-dark txt-r">
+  <div class="dropdown">
+  <a class="text-decoration-none black" data-bs-toggle="dropdown">
+  <i class="fa fa-ellipsis-v">
+  </i>
+  </a><?= $da['list']; ?>
+  <ul class="dropdown-menu">
+  <li><a data-bs-toggle="modal" data-bs-target="#edit" class="dropdown-item" href="/">Edit</a></li>
+  <li><a class="dropdown-item" href="?url=home/hapus/<?= $da['list']; ?>">Hapus</a></li>
+   </ul>
+  </div>
+  
+  <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+  <div class="modal-header">
+  <h5 class="modal-title" id="exampleModalLabel">Edit list</h5>
+  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+  </div>
+  <div class="modal-body">
+  <form method="post"	action="/?url=home/tambah">
+  <div class="input-group">
+  <input type="text" class="form-control" name="list" placeholder="Nama list" value="<?= $da['list']; ?>" aria-label="Edit data" aria-describedby="button-addon2">
+  <button class="btn btn-outline-secondary btn-success black" type="submit" id="button-addon2">Edit</button>
+  
+  </div>
+  </form></div>
+  </div>
+  </div>
+  </div>
+  
+  
+  </span>
+  </div>
+  <div class="col-5 txt-l">
+  
+  <?php if($da['status'] == 'menunggu'){ ?>
+    <a class="text-decoration-none black" href="?url=home/selesaikan/<?= $da['list']; ?>">
+    <i class="fa fa-square-o fs-2 p-1"></i>
+      </a>
+      <?php }else{ ?>
+      <i class="fa fa-check-square-o pt-1 fs-2 success"></i>
+      <?php }; ?>
+    
+    
+    </div>
+    </div>
+    <?php } }else { ?>
+ 
 
-<script type="text/javascript">
-function tambah(){
-	document.getElementById('tambah').style.display = 'block';
-}
-</script>
+ <div class="container">
+ <div class="row p-5 fs-3">
+ <center>Tidak ada artikel</center>
+</div>
+</div>
+
+<?php }; ?>
+</div>
 </body>
 </html>
