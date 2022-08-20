0ae20f71bef398282d21677fc85fa9a66bbdaa98
soifant <31540006+soifant@users.noreply.github.com>
2022-08-20 19:23:14 +0700

    Mengupgrade user.php

--- Versi MVC/app/controller/user.php
+++ Versi MVC/app/controller/user.php
@@ -24,35 +24,42 @@ class user extends Controller{
 	
 	
 	public function daftar(){
-		if(!empty($_POST['user']) || !empty($_POST['pass'])){
+		if(!empty($_POST['user']) && !empty($_POST['pass'])){
 	
-	
+		if($this->filter($_POST['user'], $_POST['pass']) > 0){
+		
 		$data['data'] = $this->model('User_model')->getMasuk($_POST);
 		foreach($data['data'] as $cek){
 		$cekU = $cek['username'];
 		}
-	
+		
 		if($cekU != $_POST['user']){
 		if($this->model('User_model')->getDaftar($_POST) > 0){
 			
-			$_SESSION['alert'] = ['sukses', 'daftar'];
+			$_SESSION['alert'] = ['sukses', 'daftar 1'];
 			return header('location:/?url=user');	
 					
 		}else{
 		
-			$_SESSION['alert'] = ['gagal', 'daftar'];
+			$_SESSION['alert'] = ['gagal', 'daftar 2'];
 			return header('location:/?url=user');			
 		}
 		
 		}else{
 		
-			$_SESSION['alert'] = ['gagal', 'daftar'];
+			$_SESSION['alert'] = ['gagal', 'daftar 3'];
 			return header('location:/?url=user/index/daftar');			
 			
 		}
 		}else{
+		
+		$_SESSION['alert'] = ['gagal', 'masukan data dengan benar dan pastikan sesuai dengan alfanumerik'];
+		return header('location:/?url=user');
+		
+		}
+		}else{
 			
-			$_SESSION['alert'] = ['gagal', 'daftar'];
+			$_SESSION['alert'] = ['gagal', 'daftar 4'];
 			return header('location:/?url=user/index/daftar');			
 			
 		}
@@ -61,30 +68,38 @@ class user extends Controller{
 	
 	
 	public function masuk(){
-		if(!empty($_POST['user']) || !empty($_POST['pass'])){
+		if(!empty($_POST['user']) && !empty($_POST['pass'])){
 		
+		if($this->filter($_POST['user'], $_POST['pass']) > 0){
 		
-		$data['data'] = $this->model('User_model')->getMasuk($_POST);
-		
-		foreach($data['data'] as $cek){
-			$cekU = $cek['username'];
-			$cekP = $cek['password'];
-		}
-		
-		if($cekU == $_POST['user'] && $cekP == $_POST['pass']){
-			$_SESSION['user'] = $cekU;
-			return header('location:/');
-		}else{
+			$data['data'] = $this->model('User_model')->getMasuk($_POST);
 			
-			$_SESSION['alert'] = ['gagal', 'masuk'];
-			return header('location:/?url=user');
+			foreach($data['data'] as $cek){
+				$cekU = $cek['username'];
+				$cekP = $cek['password'];
+			}
+			
+			if($cekU == $_POST['user'] && $cekP == md5($_POST['pass'])){
+				$_SESSION['user'] = $cekU;
+				return header('location:/');
+			}else{
+				
+				$_SESSION['alert'] = ['gagal', 'masuk'];
+				return header('location:/?url=user');
+			}
+			
+		}else{
+		
+		$_SESSION['alert'] = ['gagal', 'masukan data dengan benar dan pastikan sesuai dengan alfanumerik'];
+		return header('location:/?url=user');
 		}
 		
 		}else{
 		
-			$_SESSION['alert'] = ['gagal', 'masuk'];
-			return header('location:/?url=user');
+		$_SESSION['alert'] = ['gagal', 'masuk'];
+		return header('location:/?url=user');
 		}
+		
 		
 	}
 	
